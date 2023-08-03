55f89433cc4538420d714f2ac18c95f17801ac18
Ellen Teixeira <ellen.http4@gmail.com>
2023-08-03 16:44:46 -0300

    commit main | testando o git + intellij

---
+++ src/TestaConversao.java
@@ -0,0 +1,7 @@
+public class TestaConversao {
+    public static void main(String[] args) {
+        double salario = 1250.70;
+        int valor = (int) salario;
+        System.out.println(valor);
+    }
+}
