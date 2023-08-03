362d12cfbea4cbd8c826ba8d47c17f53872492bb
Ellen Teixeira <ellen.http4@gmail.com>
2023-08-03 17:03:44 -0300

    commit master add file java TestaConversao2

---
+++ src/TestaConversao2.java
@@ -0,0 +1,12 @@
+public class TestaConversao2 {
+    public static void main(String[] args) {
+        float pontoFlutuante = 3.14F;
+        double salario = 1270.5;
+        int valor = (int)salario;
+        System.out.println(valor);
+        double valor1 = 0.2;
+        double valor2 = 0.1;
+        double total = valor1 + valor2;
+        System.out.println(total);
+    }
+}
