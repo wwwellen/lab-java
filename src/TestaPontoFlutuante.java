b4fb3bd236628921d4be328450b87221cb0cd0c2
Ellen Teixeira <ellen.http4@gmail.com>
2023-08-03 22:37:50 -0300

    add restante dos exercícios

---
+++ src/TestaPontoFlutuante.java
@@ -0,0 +1,16 @@
+public class TestaPontoFlutuante {
+    public static void main(String[] args) {
+        double salario = 1250.70;
+        System.out.println("meu salário é " + salario);
+
+        double divisao = 3.12 / 2;
+        System.out.println(divisao);
+
+        int segundaDivisao = 5 / 2;
+        System.out.println(segundaDivisao);
+
+        double terceiraDivisao = 5.0 / 2;
+        System.out.println(terceiraDivisao);
+
+    }
+}
---
+++ src/TestaVariaveis.java
@@ -0,0 +1,8 @@
+public class TestaVariaveis {
+    public static void main (String[] args) {
+        double salario = 1250.70;
+        System.out.println(salario);
+
+        System.out.println("meu selário é " + salario);
+    }
+}
