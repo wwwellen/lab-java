63ac0a3512a910da43e780f0deb0286aea8f235c
Ellen Teixeira <ellen.http4@gmail.com>
2023-08-04 01:17:48 -0300

    usando laços de repetição

---
+++ src/TestaSomatoria.java
@@ -0,0 +1,13 @@
+public class TestaSomatoria {
+    public static void main(String[] args) {
+        int contador = 0;
+        int total= 0;
+
+        while (contador <= 10) {
+            total = total + contador;
+
+            System.out.println(total);
+            contador++;
+        }
+    }
+}
